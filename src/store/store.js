cebc4230982aac2bd7dd23972a4c6a75776a8e14
rregalado17 <rregalado17@gmail.com>
2022-08-05 23:28:26 -0500

    update lawyer reducer

--- src/store/store.js
+++ src/store/store.js
@@ -1,7 +1,5 @@
 import { configureStore } from "@reduxjs/toolkit";
-import { devToolsEnhancer } from "redux-devtools-extension";
-// import lawyerReducer from '../reducers/lawyerSlice';
-// import litigationReducer from '../reducers/litigationSlice';
+import { devToolsEnhancer } from "redux-devtools-extension"; 
 import lawyerReducer from '../reducers/lawyerReducer';
 import litigationReducer from "../reducers/litigationReducer";
 
